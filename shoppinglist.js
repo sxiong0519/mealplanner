8ca7b508b071ec8febb9b74bf6fa00f8110d972a
sxiong0519 <sxiong0519@gmail.com>
2021-05-27 16:01:39 -0500

    adding before EOD

---
+++ shoppinglist.js
@@ -0,0 +1,33 @@
+const groceryItems = [
+    {
+        id: 1,
+        item: "eggs",
+        price: 3,
+        area: "fridge"
+    },
+    {
+        id: 2,
+        item: "coffee",
+        price: 8,
+        area: "shelf"
+    },
+    {
+        id: 3,
+        item: "rice",
+        price: 30,
+        area: "pantry closet"
+    },
+    {
+        id: 4,
+        item: "peaches",
+        price: 4,
+        area: "shelf"
+    },
+    {
+        id: 5,
+        item: "chicken",
+        price: 7,
+        area: "fridge"
+    }
+    
+]
